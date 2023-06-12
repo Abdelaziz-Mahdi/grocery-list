a7f86d0fe03c2be8480ebbac0c8faa266ac43037
Abdelaziz Mohamed Ali <107482707+Abdelaziz-Ali@users.noreply.github.com>
2023-06-13 01:32:10 +0300

    JSX atrribute

--- src/Components/Products.js
+++ src/Components/Products.js
@@ -1,4 +1,9 @@
 let productCount = 0;
+let imageUrl = require('../images/milk.jpg');
+let style = {
+  padding: "0px 10px",
+  fontSize: "20px",
+};
 
 function displayProductCount() {
   return productCount > 0 ? productCount : "Zero";
@@ -16,14 +21,14 @@ function Products() {
               fuga autem maiores necessitatibus.
             </p>
             <div className='d-flex align-items-center justify-content-between mt-1'>
-              <h6 className='font-weight-bold my-2'>$120.00</h6>
+              <h6 className='font-weight-bold my-2' style={ { 'marginRight': "30px"} }>$120.00</h6>
               <button className='btn btn-primary'>-</button>
-              <span>{displayProductCount()}</span>
+              <span style={style}>{displayProductCount()}</span>
               <button className='btn btn-primary'>+</button>
             </div>
           </div>
           <img
-            src=''
+            src={imageUrl}
             alt='Generic placeholder'
             width='200'
             className='ml-lg-5 order-1 order-lg-2'
