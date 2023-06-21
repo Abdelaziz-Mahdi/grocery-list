746a0539d2b9eeac34b2b7f9a42a3b795ac2ac22
Abdelaziz Mohamed Ali <107482707+Abdelaziz-Ali@users.noreply.github.com>
2023-06-21 05:06:54 +0300

    Rendering conditional content

--- src/Components/ProductList/ProductList.js
+++ src/Components/ProductList/ProductList.js
@@ -1,20 +1,26 @@
 import Products from './Products';
 
-
-
 function ProductList(props) {
-  return (
-        <ul className='list-group shadow'>
-          {props.setProductHandler.map((product) =>{return (<Products
-              key={product.pID}
-              id={product.pID}
-              name={product.pName}
-              description={product.desc}
-              isAvailable={product.isAvailable}
-              imageUrl={product.image}
-              price={product.price}
-            />);})}
-        </ul>
+  return props.setProductHandler.length === 0 ? (
+    <div className='alert alert-danger'>
+      <h3>No Products Found</h3>
+    </div>
+  ) : (
+    <ul className='list-group shadow'>
+      {props.setProductHandler.map((product) => {
+        return (
+          <Products
+            key={product.pID}
+            id={product.pID}
+            name={product.pName}
+            description={product.desc}
+            isAvailable={product.isAvailable}
+            imageUrl={product.image}
+            price={product.price}
+          />
+        );
+      })}
+    </ul>
   );
 }
 export default ProductList;
